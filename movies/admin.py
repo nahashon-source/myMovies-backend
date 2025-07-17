50bd1de446c66a1376b5ef0ee4bf49f5e41a8d16
nahashon <nashonmwendwa0@gmail.com>
2025-07-17 12:16:29 +0300

    Register Movie model in admin panel with filters and search

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class MOvieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'release_year', 'created_at')
+    list_filter = ('category', 'release_year')
+    search_fields = ('title', 'description')
